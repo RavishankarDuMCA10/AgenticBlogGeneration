d42a2769532a4c3ff99021f7499c5efa486426e4
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2026-03-13 20:48:27 +0530

    Integrating langgraph studion for debugging.

--- src/llms/groqllm.py
+++ src/llms/groqllm.py
@@ -6,10 +6,16 @@ from dotenv import load_dotenv
 class GroqLLM:
     def __init__(self):
         load_dotenv()
+        api_key = os.getenv("GROQ_API_KEY")
+
+        if not api_key:
+            raise ValueError("GROQ_API_KEY is missing. Check your .env file.")
+
+        os.environ["GROQ_API_KEY"] = api_key
 
     def get_llm(self):
         try:
-            os.environ["GROQ_API_KEY"] = self.groq_api_key = os.getenv("GROQ_API_KEY")
+            self.groq_api_key = os.getenv("GROQ_API_KEY")
             llm = ChatGroq(api_key=self.groq_api_key, model="llama-3.1-8b-instant")
             return llm
         except Exception as e:
